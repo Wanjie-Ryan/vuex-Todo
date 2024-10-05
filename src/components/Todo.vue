f76eaf7d27d7c6a8f0cfd2fa24565f349ed6c92f
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-10-05 20:39:58 +0300

    todos vuex

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="container">
     <h4>Todos</h4>
-    
+
     <div class="todos">
       <div class="todo" v-for="todo in allTodos" :key="todo.id">
         {{ todo.title }}
+        <p class="del" @click="deleteTodo">Del</p>
       </div>
     </div>
   </div>
@@ -18,34 +19,40 @@ export default {
   computed: {
     ...mapGetters(["allTodos"]),
   },
-  methods:{
-    ...mapActions(['FetchTodos'])
+  methods: {
+    ...mapActions(["FetchTodos", "deleteTodo"]),
   },
 
   // Fetch Todos is called in the created hook which is just before the component loads or created.
-  created(){
+  created() {
     this.FetchTodos();
-  }
+  },
 };
 </script>
 
 <style scoped>
-
-.todos{
-    display:grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap:1rem;
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
 }
 
-.todo{
-    border:1px solid #ccc;
-    background-color: #41b883;
-    padding:1rem;
-    border-radius: 5px;
-    text-align: center;
-    position: relative;
-    cursor:pointer;
+.todo {
+  border: 1px solid #ccc;
+  background-color: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
 }
 
-
+.del {
+  position: absolute;
+  bottom: 5px;
+  right: 10px;
+  font-weight: bold;
+  color: red;
+  cursor: pointer;
+}
 </style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -24,21 +24,30 @@ const actions = {
   // there in the actions, we fetch the todos, then the commit is used when we want to pass the data to the mutations which then update the state,,,, we pass the name of the mutation and the data fetched.
 
   // commit part of a large object that is passed in
-  async createTodo({commit}, title){
+  async createTodo({ commit }, title) {
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/todos",
+      { title, completed: false }
+    );
+    console.log(response);
+    commit("newTodo", response.data);
+  },
 
-    const response = await axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed:false})
-    console.log(response)
-    commit('newTodo', response.data)
-
-
-  }
-
+  async deleteTodo({ commit }, id) {
+    const response = await axios.delete(
+      `https://jsonplaceholder.typicode.com/todos/${id}`
+    );
+    console.log(response);
+    commit("removeTodo", id);
+  },
 };
 
 const mutations = {
   // here in the mutations, setTodos is the name of the mutation, then state, is the state, then also pass in the expected data, then we set the state to the data passed in.
   setTodos: (state, data) => (state.todos = data),
-  newTodo:(state, todo) => state.todos.unshift(todo)
+  newTodo: (state, todo) => state.todos.unshift(todo),
+  removeTodo: (state, id) =>
+    (state.todos = state.todos.filter((todo) => todo.id !== id)),
 };
 
 export default {
