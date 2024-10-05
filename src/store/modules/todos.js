edef68b68a19357c3c3846362e3e3de7f4f07812
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-10-05 19:18:28 +0300

    todos vuex

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,43 +1,37 @@
-import axios from 'axios'
-
+import axios from "axios";
 
 const state = {
-    todos:[]
-}
+  todos: [],
+};
 
 const getters = {
-    allTodos(state){
-        return state.todos
-    }
-}
+  allTodos(state) {
+    return state.todos;
+  },
+};
 
 const actions = {
+  // we do not call the mutation directly, we call it using the commit
+  //{commit}
+  async FetchTodos({ commit }) {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos"
+    );
+    console.log(response.data);
+    commit("setTodos", response.data);
+  },
 
-    // we do not call the mutation directly, we call it using the commit
-    //{commit}
-    async FetchTodos({commit}){
-
-        const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
-        console.log(response.data)
-        commit('setTodos', response.data)
-
-    }
-
-    // there in the actions, we fetch the todos, then the commit is used when we want to pass the data to the mutations which then update the state,,,, we pass the name of the mutation and the data fetched.
-    
-
-}
+  // there in the actions, we fetch the todos, then the commit is used when we want to pass the data to the mutations which then update the state,,,, we pass the name of the mutation and the data fetched.
+};
 
 const mutations = {
-
-    // here in the mutations, setTodos is the name of the mutation, then state, is the state, then also pass in the expected data, then we set the state to the data passed in.
-    setTodos: (state, data) => (state.todos = data)
-
-}
+  // here in the mutations, setTodos is the name of the mutation, then state, is the state, then also pass in the expected data, then we set the state to the data passed in.
+  setTodos: (state, data) => (state.todos = data),
+};
 
 export default {
-    state,
-    getters,
-    actions,
-    mutations
-}
+  state,
+  getters,
+  actions,
+  mutations,
+};
